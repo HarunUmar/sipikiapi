5e567977c5cf70b58d50554a35707650bd53a46c
HarunUmar <arunumar21@gmail.com>
2018-06-25 22:29:58 +0800

    wau

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_secure_password
   belongs_to :eselon
   belongs_to :city
   belongs_to :pemkot
@@ -18,9 +17,4 @@ class User < ApplicationRecord
   						:hp ,
   						:message => 'masih kosong'
 
- def self.from_token_payload(payload)
-    self.find payload["sub"]
-  end
-
-  
 end
